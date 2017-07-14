ea82c8a05de2248d33afc9964ffbf6061866bfd7
Alexandre Chaussier <alexandre.chaussier@temelio.com>
2017-07-14 08:34:44 +0200

    WIP - First version

--- tests/test_installation.py
+++ tests/test_installation.py
@@ -2,14 +2,41 @@
 Role tests
 """
 
+import pytest
 from testinfra.utils.ansible_runner import AnsibleRunner
 
 testinfra_hosts = AnsibleRunner('.molecule/ansible_inventory').get_hosts('all')
 
 
-def test_foo_a(User):
-    assert User().name == 'root'
+@pytest.mark.parametrize('name', [
+    ('openjdk-8-jdk'),
+])
+def test_packages(host, name):
+    """
+    Test installed packages
+    """
+
+    assert host.package(name).is_installed
 
 
-def test_foo_b(User):
-    assert User().name == 'root'
+def test_user(host):
+    """
+    Test zeppelin user
+    """
+
+    app_user = host.user('zeppelin')
+    assert app_user.exists
+
+
+@pytest.mark.parametrize('folder_path,user,group,mode', [
+    (''),
+])
+def test_folders(host, folder_path, user, group, mode):
+    """
+    Test app paths
+    """
+
+    current_folder = host.file(folder_path)
+
+    assert current_folder.exists
+    assert current_folder.is_directory
